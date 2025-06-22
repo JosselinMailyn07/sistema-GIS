0c3bb9e09089062bab05783c46a1c928b8adb5fc
cesarndrs <60056648+cesarndrs@users.noreply.github.com>
2025-06-22 17:33:22 -0500

    se agregaron los apartados de inventario, aun en desarrollo

--- src/components/layout/AdminSidebar.tsx
+++ src/components/layout/AdminSidebar.tsx
@@ -212,17 +212,61 @@ const adminMenuItems: MenuItem[] = [
     ]
   },
   {
-    id: 'Inventario',
+    id: 'inventario',
     title: 'Inventarios',
-    icon: Users,
-    path: '/admin/clientes',
-    children: [
+    icon: FileText,
+    children:  [
       {
         id: 'recepciones-bodega',
         title: 'Recepciones de Bodega',
         icon: UserCheck,
-        path: '/admin/recepciones-bodega'
+        path: '/admin/recepciones-bodegas'
       },
+      {
+        id: 'movimientos-productos',
+        title: 'Movimientos de productos',
+        icon: UserCheck,
+        path: '/admin/movimientos-productos'
+      },
+      {
+        id: 'transferencias-bodegas',
+        title: 'Transferencias de Bodegas',
+        icon: UserCheck,
+        path: '/admin/transferencias-bodegas'
+      },
+      {
+        id: 'guias-remision',
+        title: 'Guias de Remision',
+        icon: UserCheck,
+        path: '/admin/guias-remision'
+      },
+      {
+        id: 'revoluciones-lotes',
+        title: 'Revoluciones por Lotes',
+        icon: UserCheck,
+        path: '/admin/revoluciones-lotes'
+      },
+      {
+        id: 'inventario-inicial-fisico',
+        title: 'Inventario Inicial | Fisico',
+        icon: UserCheck,
+        path: '/admin/inventario-inicial-fisico'
+      },
+      {
+        id: 'grupo-productos',
+        title: 'Grupos de Productos',
+        icon: UserCheck,
+        path: '/admin/grupo-productos'
+      },
+      {
+        id: 'bodega',
+        title: 'Bodega',
+        icon: UserCheck,
+        path: '/admin/bodaga'
+      },
+      
+
+      
 // Agregar modulos faltantes de clientes
 
 
@@ -297,7 +341,7 @@ export const AdminSidebar = ({ isOpen }: AdminSidebarProps) => {
   // Determinar la sección activa basada en la ruta
   const activeSection = location.pathname.startsWith('/admin/ventas') ? 'ventas' :
                        location.pathname.startsWith('/admin/gastos') ? 'gastos' :
-                       location.pathname.startsWith('/admin/clientes') ? 'clientes' : null;
+                       location.pathname.startsWith('/admin/clientes') ? 'clientes'  : null;
   /**
    * Función para expandir/contraer elementos del menú
    * @param itemId - ID del elemento a expandir/contraer
