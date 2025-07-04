157c45fffd9963cf9fe6b09a7d5cbb59e1c484ac
Hans-el <halcivar3030@utm.edu.ec>
2025-07-04 14:05:35 -0500

    Actualizar de íconos a el uso de Tabs para las pestañas

--- src/components/admin/configuracion/archivo/provincias/provincias.tsx
+++ src/components/admin/configuracion/archivo/provincias/provincias.tsx
@@ -1,1 +1,30 @@
-//provincias
+//Formulario de provincias
+// Aqui debe ir el formulario que está en la maquina virtual. Debe quedar con las mismas funcionalidades.
+// Este formulario no está completado finalmente, ni tampoco bien estrcuturado.
+// Pero mas o menos así debe ser el formulario. 
+
+import React from 'react';
+import { Button } from '@/components/ui/button';
+
+export const ProvinciasForm = () => (
+ 
+ <div className="mt-4 p-4 border rounded-lg shadow-md">
+    <h3 className="text-lg font-semibold mb-4">Provincias</h3>
+    <form>
+        
+      <div className="mb-4">
+        <label className="block text-sm font-medium text-gray-700">Buscar por Ruc, Nombre, Id</label>
+        <input type="text" className="mt-1 block w-full border-gray-300 rounded-md shadow-sm" />
+      </div>
+      <div className="grid grid-cols-2 gap-4 mb-4">
+      </div>
+      
+      <div className="flex space-x-2">
+        <Button type="button" variant="outline">Nuevo</Button>
+        <Button type="button" variant="outline">Eliminar</Button>
+        <Button type="button" variant="outline">Actualizar</Button>
+        <Button type="button" variant="outline">Restaurar</Button>
+      </div>
+    </form>
+  </div>
+); 
